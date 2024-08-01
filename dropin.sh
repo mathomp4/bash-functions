ab8813af0e4ca53869bb33973c414332cb9b8a86
Matthew Thompson <matthew.thompson@nasa.gov>
2024-08-01 08:16:16 -0400

    Use the right number of tasks

--- dropin.sh
+++ dropin.sh
@@ -1,4 +1,8 @@
 # vim: ft=bash 
 function dropin () {
-   srun --pty --jobid=$1 bash
+   JOBID=$1
+   # We need to grab the number of tasks from the
+   # job id so we can use that with srun
+   NUMTASKS=$(scontrol show job $JOBID | rg -o 'NumTasks=\d+' | cut -d= -f2)
+   srun -n $NUMTASKS --pty --jobid=$JOBID bash
 }
